19298a71938213210127271a672e92e6fb4d0ae9
Emefak1 <mbukestheremefak@gmail.com>
2023-03-16 15:17:37 -0700

    Puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <studio.h>
-/**
+/*
  * main - A program that prints a line with puts function
+ *
  * Return: 0 (Success)
- */
+*/
 int main(void)
 {
 puts("\"Programming is like buildng a multilingual puzzle");
